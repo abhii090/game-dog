420f44cddb6ce3a7835a1ce7bbef935f5aefdf62
Abhishek Bedsur <abhishek.bedsur@indivillage.co>
2025-10-27 17:01:03 +0530

    done tilll food flight

---
+++ Assets/Scenes/scripts/Playercontroller.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float HorizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    public GameObject projectileprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectileprefab, transform.position, projectileprefab.transform.rotation);  
+            // launch a projectile from the player
+        }
+    }
+}
