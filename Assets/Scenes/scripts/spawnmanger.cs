aacb1b6d8ef59edff6e485e39451a5e67cede372
Abhishek Bedsur <abhishek.bedsur@indivillage.co>
2025-10-28 15:57:31 +0530

    modified spawnx axis y axis

--- Assets/Scenes/scripts/spawnmanger.cs
+++ Assets/Scenes/scripts/spawnmanger.cs
@@ -6,7 +6,8 @@ public class spawnmanger : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] animalprefabs;
-    
+    public float spawnRangeX = 10;
+    public float spawnPosZ = 10;
     void Start()
     {
 
@@ -17,9 +18,10 @@ public class spawnmanger : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0 , spawnPosZ);
             int animalIndex = Random.Range(0, animalprefabs.Length);
         
-            Instantiate(animalprefabs[animalIndex], new Vector3(0, 0, 20),
+            Instantiate(animalprefabs[animalIndex], spawnPos,
 
         animalprefabs[animalIndex].transform.rotation);
         }
